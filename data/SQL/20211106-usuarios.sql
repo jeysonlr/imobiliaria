4d61ec4282e131583c480816bbc16a8e500b48e2
jeysonlr <jeysonlr@gmail.com>
2021-12-02 14:20:00 -0300

    Realizado crud especificado no pedido da atividade

---
+++ data/SQL/20211106-usuarios.sql
@@ -0,0 +1,24 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+CREATE TABLE `usuarios` (
+  `codigo` int NOT NULL,
+  `usuario` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  `senha` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`codigo`);
+
+
+ALTER TABLE `usuarios`
+  MODIFY `codigo` int NOT NULL AUTO_INCREMENT;
+
+INSERT INTO usuarios(usuario, senha) values ('root', MD5('123'));
+COMMIT;
+
---
+++ data/SQL/20211130-imoveis.sql
@@ -0,0 +1,17 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `imoveis` (
+  `matricula` int NOT NULL,
+  `nome` varchar(80) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `imoveis`
+    ADD PRIMARY KEY (`matricula`);
+
+ALTER TABLE `imoveis`
+    MODIFY `matricula` int NOT NULL AUTO_INCREMENT;
+
+COMMIT;
